9738c93066e7c9e4267aa0392d0e50eeaa23a809
Courtney Campbell <courtcam1995@gmail.com>
2022-12-11 16:44:12 +0000

    made adjustment to script and added comments for each line

--- scripts/ApacheScript.sh
+++ scripts/ApacheScript.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+
+# updaing all the packages on the server 
 yum update -y
+
+# installing the Apache web server
 yum install httpd -y
+
+# Start the Apache services  
 systemctl start httpd
+
+# Enable to Apache services to start automatically whenever server restart or boot
 systemctl enable httpd
 
+# Create a same HTML file called index.html and placed into in the /var/www/html location.
 echo "<html><body>Hello World - My Name is Courtney Campbell and this is a Custom Webpage using Apache<body><html>" >> /var/www/html/index.html
-
-
